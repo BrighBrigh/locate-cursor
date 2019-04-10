5fa807cd1db1e2701f7f1da3d656edd8fd4ded64
Brian Winkler <brianewinkler@gmail.com>
2019-04-10 10:03:10 -0700

    Update locate-cursor.sh

--- locate-cursor.sh
+++ locate-cursor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 RUN=1
+ORIGINAL=$(gsettings get org.gnome.desktop.interface cursor-size)
 
 function getX(){
     local  __resultvar=$1
@@ -18,7 +19,7 @@ function calculateDistance() {
     local __result=$1
     getX X1
     getY Y1
-    sleep 0.05
+    sleep 0.02
     getX X2
     getY Y2
     SQUARE=$(bc <<< "($X1- $X2)*($X1- $X2) + ($Y1 - $Y2)*($Y1 - $Y2)")
@@ -26,24 +27,39 @@ function calculateDistance() {
     eval $__result="$DISTANCE"
 }
 
+function sleepMode(){
+  SLEEP=0
+  while [ $SLEEP -lt 1 ]
+  do
+    sleep 0.5
+    calculateDistance SLEEP
+  done
+}
+
 while [ $RUN -gt 0 ]
 do
     i=0
     PASS=0
-
-    while [ $i -lt 10 ]
+    SLEEPMODE=0
+    while [ $i -lt 5 ]
     do
       calculateDistance DISTANCE
-      if [ $DISTANCE -gt 300 ]; then
+      if [ $DISTANCE -eq 0 ]; then
+        ((SLEEPMODE++))
+      fi
+      if [ $SLEEPMODE -eq 4 ]; then
+        sleepMode
+      fi
+      if [ $DISTANCE -gt 100 ]; then
         ((PASS++))
       fi
       ((i++))
     done
 
-    if [ $PASS -gt 5 ]; then
+    if [ $PASS -gt 2 ]; then
       gsettings set org.gnome.desktop.interface cursor-size 115
       sleep 1
-      gsettings set org.gnome.desktop.interface cursor-size 24
+      gsettings set org.gnome.desktop.interface cursor-size $ORIGINAL
     fi
 
 done
